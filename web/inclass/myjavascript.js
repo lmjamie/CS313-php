b3b579d2dae59fcea43dd5d107d21e573740eb1f
Landon Jamieson <landonjamieson@gmail.com>
2017-04-19 14:46:31 -0600

    fix

--- web/inclass/myjavascript.js
+++ web/inclass/myjavascript.js
@@ -4,5 +4,6 @@ function been_clicked() {
 
 function change_color(color){
     var div1 = document.getElementById("div1");
-    div1.style.backgroundColor = color.value;
+    var divColor = document.getElementById("color");
+    div1.style.backgroundColor = divColor;
 }
