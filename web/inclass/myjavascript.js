9c4822a59ff199e5cbc9970d1522426c1e87d2da
Landon Jamieson <landonjamieson@gmail.com>
2017-04-19 14:40:38 -0600

    first try

--- web/inclass/myjavascript.js
+++ web/inclass/myjavascript.js
@@ -1,3 +1,8 @@
 function been_clicked() {
     alert("Clicked!");
 }
+
+function change_color(color){
+    var div1 = document.getElementById("div1");
+    div1.style.backgroundColor = color.value;
+}
